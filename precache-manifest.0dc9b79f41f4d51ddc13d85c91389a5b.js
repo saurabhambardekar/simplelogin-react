4eb44ebc20f8ccb9c3fe65fb95c1bfc97f75a8c0
saurabhambardekar <sau.manu07@gmail.com>
2020-05-18 22:09:20 +0530

    Updates

---
+++ precache-manifest.0dc9b79f41f4d51ddc13d85c91389a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef7019507b2dabed10e2302e024e4d7",
+    "url": "/simplelogin-react/index.html"
+  },
+  {
+    "revision": "92a8faa3b75379d98320",
+    "url": "/simplelogin-react/static/css/main.7a517673.chunk.css"
+  },
+  {
+    "revision": "1ece6c262db697cd86b8",
+    "url": "/simplelogin-react/static/js/2.cc247b6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simplelogin-react/static/js/2.cc247b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a8faa3b75379d98320",
+    "url": "/simplelogin-react/static/js/main.a5be7fd9.chunk.js"
+  },
+  {
+    "revision": "1a8531f5351b05a779d1",
+    "url": "/simplelogin-react/static/js/runtime-main.737375cc.js"
+  }
+]);
